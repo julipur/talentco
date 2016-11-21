efa3e6f85ca7796c8e45aa455500b8f403f77b7e
Qudoos <abdul.qudoos@gmail.com>
2016-11-20 21:06:27 -0500

    layout

--- TalentConnect.UI/Global.asax.cs
+++ TalentConnect.UI/Global.asax.cs
@@ -15,7 +15,7 @@ namespace TalentConnect.UI
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
