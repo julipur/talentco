3b98f22c101de8bc508833ddf2b621e4f14d13bf
Qudoos <abdul.qudoos@gmail.com>
2017-02-12 11:04:22 -0500

    wip

---
+++ TalentConnect.UI/Domain/Job.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalentConnect.UI.Domain
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string JobType { get; set; }
+        public int YearsOfExperience { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ TalentConnect.UI/Infrastructure/TalentConnectContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TalentConnect.UI.Domain;
+
+namespace TalentConnect.UI.Infrastructure
+{
+    public class TalentConnectContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
