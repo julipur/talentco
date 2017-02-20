4bd714c39be52e160ef2893a93bfa22cf6072910
Qudoos <abdul.qudoos@gmail.com>
2017-02-19 20:08:20 -0500

    navigation

--- TalentConnect.UI/Infrastructure/Navigation.cs
+++ TalentConnect.UI/Infrastructure/Navigation.cs
@@ -7,12 +7,53 @@ namespace TalentConnect.UI.Infrastructure
 {
     public class Navigation
     {
+        public class About
+        {
+            public string Controller = "about";
+            public string Index = "index";
+        }
+
+        public class Candidates
+        {
+            public string Controller = "candidates";
+            public string Index = "index";
+        }
+
+        public class Contact
+        {
+            public string Controller = "contact";
+            public string Index = "index";
+        }
+
+        public class Employers
+        {
+            public string Controller = "employers";
+            public string Index = "index";
+        }
+
+        public class Home
+        {
+            public string Controller = "home";
+            public string Index = "index";
+        }
+
         public class Jobs
         {
             public string Controller = "jobs";
             public string Index = "index";
-            public string ManageJobs = "manage-jobs";
+            public string ManageJobs = "manage";
+        }
 
+        public class Privacy
+        {
+            public string Controller = "privacy";
+            public string Index = "index";
+        }
+
+        public class Terms
+        {
+            public string Controller = "terms";
+            public string Index = "index";
         }
     }
 }
